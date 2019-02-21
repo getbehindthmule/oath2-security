bb0a372e268c9ee551ccf8fa0c398ea19440ac28
getbehindthmule <gerardsavage@me.com>
2019-02-21 16:24:42 +0000

    add infrastructure for auditing

---
+++ src/main/java/com/greenhills/oauth2security/audit/SpringSecurityAuditorAware.java
@@ -0,0 +1,25 @@
+package com.greenhills.oauth2security.audit;
+
+
+import com.greenhills.oauth2security.model.security.User;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+
+import java.util.Optional;
+
+class SpringSecurityAuditorAware implements AuditorAware<User> {
+
+    public Optional<User> getCurrentAuditor() {
+
+        return Optional.ofNullable(SecurityContextHolder.getContext())
+                .map(SecurityContext::getAuthentication)
+                .filter(Authentication::isAuthenticated)
+                .map(Authentication::getPrincipal)
+                .map(User.class::cast);
+    }
+}
+
+
